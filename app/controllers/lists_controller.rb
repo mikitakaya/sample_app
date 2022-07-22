e3e30821735c75a6a14f905eda5362f25ee1e215
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-07-22 09:42:22 +0000

    [Add]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -36,7 +36,7 @@ class ListsController < ApplicationController
   def destroy
    list = List.find(params[:id]) # データ（レコード）を1件取得
    list.destroy # データ（レコード）を削除
-   redirect_to '/list' # 投稿一覧画面へリダイレクト
+   redirect_to '/lists' # 投稿一覧画面へリダイレクト
   end
 
   private
